3184d19f6fa9a8333a6b20d74ddbd34c8df021b3
Seong Lee <selee@allegisgroup.com>
2018-03-19 14:12:43 -0400

    Retrieve artist and songs from submissions

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ def main():
                         client_id = config.reddit_client_id,
                         client_secret = config.reddit_client_secret)
 
+    subreddit = reddit.subreddit("Music")
+    for submission in subreddit.submissions():
+        processSubmission(submission)
+
+def processSubmission(submission):
+    submission_title = submission.title.split("-")
+    artist = submission_title[0].strip()
+    song = ""
+    
+    if len(submission_title) > 1:
+        song = submission.title.split("-")[1].strip()
+
+    #print(artist)
+    print(song)
 
 if __name__ == '__main__':
     main()
